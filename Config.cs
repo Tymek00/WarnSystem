6da790e465b3ac116e8fa1380a93872f42531995
Tymek00 <tyja3017@gmail.com>
2025-04-04 23:00:00 +0200

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace WarnSystem
+{
+    public class Config : IConfig
+    {
+        [Description("ON/OFF")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Debug Mode")]
+        public bool Debug { get; set; } = false;
+
+        [Description("Nazwa pliku do przechowywania danych warnów/notatek w folderze PluginData.")]
+        public string DataFileName { get; set; } = "WarnData.json";
+
+        [Description("Wymagana permisja do używania warnów.")]
+        public string RequiredPermission { get; set; } = "perm.warn.adm";
+
+        [Description("Główna nazwa komendy, której używa plugin.")]
+        public string CommandPrefix { get; set; } = "warn";
+
+        [Description("Aliasy komendy (alternatywne nazwy)")]
+        public string[] CommandAliases { get; set; } = { "ws", "warns" };
+    }
+}
---
+++ WarnPlugin.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using WarnSystem.Handlers;
+
+using ServerEvents = Exiled.Events.Handlers.Server;
+
+namespace WarnSystem
+{
+    public class WarnPlugin : Plugin<Config>
+    {
+        public override string Name => "WarnSystem";
+        public override string Author => "Tymek";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(9, 5, 1);
+
+        public static WarnPlugin Instance { get; private set; }
+
+        public DataHandler DataHandler { get; private set; }
+        private EventHandlers _eventHandlers;
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            DataHandler = new DataHandler(this);
+            _eventHandlers = new EventHandlers(this);
+
+            ServerEvents.WaitingForPlayers += _eventHandlers.OnWaitingForPlayers;
+            ServerEvents.RestartingRound += _eventHandlers.OnRestartingRound;
+
+            Log.Info($"{Name} v{Version} by {Author} został załadowany.");
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            ServerEvents.WaitingForPlayers -= _eventHandlers.OnWaitingForPlayers;
+            ServerEvents.RestartingRound -= _eventHandlers.OnRestartingRound;
+
+            DataHandler?.SaveData(); 
+            Log.Info($"{Name} został wyłączony. Zapisano dane (jeśli były dostępne).");
+
+            _eventHandlers = null;
+            DataHandler = null;
+            Instance = null;
+            base.OnDisabled();
+        }
+    }
+
+    public class EventHandlers
+    {
+        private readonly WarnPlugin _plugin;
+        public EventHandlers(WarnPlugin plugin) => _plugin = plugin;
+
+        public void OnWaitingForPlayers()
+        {
+            Log.Debug("WaitingForPlayers: Ładowanie danych...");
+            _plugin.DataHandler?.LoadData();
+        }
+
+        public void OnRestartingRound()
+        {
+            Log.Debug("RestartingRound: Zapisywanie danych...");
+            _plugin.DataHandler?.SaveData();
+        }
+
+    }
+}
